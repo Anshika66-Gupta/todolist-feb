0b30d56e9ae217a7102c8c41f455145cc5810d99
“Anshika <ansgupta.01@gmail.com>
2023-03-27 19:16:28 +0530

    add-items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
-
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem)
+  }
   render() {
     return (
       <div className='App'>
       <header>
-        <form id='to-do-form'>
-          <input type="text" placeholder="Enter Text" />
+        <form id='to-do-form'  onSubmit={this.addItem} >
+            <input type="text" placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
           <button type='submit'>Add</button>
         </form>
         </header>
